0a5e1758c316f0ffc513dd741d0146d5c71e96f2
Ajanthy <30925320+Ajanthy@users.noreply.github.com>
2019-08-17 04:01:28 +0530

    Update models.py

--- loginproject/loginwithauth/models.py
+++ loginproject/loginwithauth/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+#from django.utils import timezone
 
 class admin(models.Model):
     id = models.AutoField(primary_key=True)
@@ -16,26 +17,51 @@ class user(models.Model):
     nickname = models.CharField(max_length=300)
     email = models.CharField(max_length=300)
     phoneNo = models.CharField(max_length=100)
+    date_created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
 
     class Meta:
             db_table = 'user'
+    
+    def __str__(self):
+        return self.nic
 
-class reviews(models.Model):
+class review(models.Model):
     id = models.AutoField(primary_key=True)
     user_id = models.IntegerField()
     queAndAnsr = models.TextField()
     geo_tag = models.TextField()
     device_signature= models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
 
     class Meta:
-            db_table = 'reviews'
+            db_table = 'review'
+
+    def __str__(self):
+        return str(self.user_id)
+    
+    # @property
+    # def days_since_creation(self):
+    #     diff = timezone.now() - self.date_created
+    #     return diff.days
 
 
-class complaints(models.Model):
+class complaint(models.Model):
     id = models.AutoField(primary_key=True)
     user_id = models.IntegerField()
     geo_tag = models.TextField()
     description= models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True) 
 
     class Meta:
-            db_table = 'complaints'
+            db_table = 'complaint'
+
+    def __str__(self):
+        return str(self.user_id)
+
+    # @property
+    # def days_since_creation(self):
+    #     diff = timezone.now() - self.date_created
+    #     return diff.days
